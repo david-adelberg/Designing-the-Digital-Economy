3d6a466b66c81ab357671f3948b5dd98b300f3ce
Alan Liu <alanl.liu@yahoo.com>
2016-12-04 17:21:53 -0500

    added lines for routes and pretty colors

---
+++ findPath.py
@@ -0,0 +1,219 @@
+import numpy as np
+import itertools as it
+# import matplotlib.pyplot as plt
+
+# The below code is slightly modified from the version in the abstract locatiosn to reflect that our list of destinations will look like $$[origin, loc_1, loc_2, \ldots]$$
+
+# In[11]:
+
+def pathList(destinations):
+    perms = it.permutations(destinations[1:len(destinations)])
+    origin = destinations[0]
+    lst = list(perms)
+    np.random.shuffle(lst)
+
+    #To reduce computation time in the case of a large number of riders,
+    #We simply look at some large subset of paths instead of the entire space
+    #Obviously this isn't great in the worst case, but it is good in the average case
+    if len(lst) > 120:
+        lst = lst[:120]
+    permlist = []
+    for x in lst:
+        newpath = []
+        newpath.append(origin)
+        for j in x:
+            newpath.append(j)
+        permlist.append(newpath)
+
+    return permlist
+
+def subsetPathList(destinations, leftOut):
+    subDests = []
+    for x in destinations:
+        if x[2] != leftOut:
+            subDests.append(x)
+
+    return pathList(subDests)
+
+
+# This code is copied from the abstract location version; nothing has changed.
+
+# In[12]:
+
+# Compute the cost of a given path given a distance matrix
+# containing pairwise distances between points in the path
+def costMat(weights, path, distances):
+    currloc = path[0]
+    currcost = 0
+    for x in path:
+        currcost += weights[x[2]]
+    totalcost = 0
+    for i in range(len(path)):
+        totalcost += currcost * distances[currloc[2]][path[i][2]]
+        if i > 0:
+            currcost = currcost - weights[path[i][2]]
+        currloc = path[i]
+    return totalcost
+
+# Compute the environmental cost of the path, equal to
+# the cost of the path to the car
+def envCostMat(path, distances):
+    currloc = path[0]
+    currcost = carCost
+    totalcost = 0
+    for i in range(len(path)):
+        if i > 0:
+            totalcost += currcost * distances[currloc[2]][path[i][2]]
+            currloc = path[i]
+    return totalcost
+
+# Computes the individual costs of a given path and returns a matrix
+# with the respective costs formatted as [car, 0, 1, . . . ]
+def indivCostMat(weights, path, distances):
+    numPeople = len(weights)
+    costs = np.zeros(numPeople)
+    currloc = path[0]
+    totalcost = 0
+    inCar = set()
+    inCar.add(0)
+    for n in range(len(path)):
+        inCar.add(path[n][2])
+    for i in range(len(path)):
+        for j in inCar:
+            costs[j] += weights[j]*distances[currloc[2]][path[i][2]]
+
+        if i > 0: #Never removes the car
+            inCar.remove(path[i][2])
+        currloc = path[i]
+    return costs
+
+def optimalPath(weights, paths, distances):
+    wgtcost = []
+    for i in range(len(paths)):
+        wgtcost.append(costMat(weights,paths[i],distances))
+
+    optimal = wgtcost.index(min(wgtcost))
+    return optimal
+
+def shortestPath(paths,distances):
+    unwgtcost = []
+    for i in range(len(paths)):
+        unwgtcost.append(envCostMat(paths[i],distances))
+
+    shortest =  unwgtcost.index(min(unwgtcost))
+    return shortest
+
+
+# This code is unique to calculating payments, and is unfinished. I think it works as desired, although you may want to compare and experiment with the less polished code in the GlenProject-16-11-13 file to make sure that this does what you want.
+
+# In[13]:
+
+def harmMatrices(timeValues, destinations, distMatrix):
+    #distMatrix = distanceMatrix(destinations)
+    allPaths = pathList(destinations)
+    #allPaths = transform_path_list(allPaths)
+    optPath = optimalPath(timeValues,allPaths,distMatrix)
+    shortPath = shortestPath(allPaths,distMatrix)
+    shortPath = allPaths[shortPath]
+    optPath = allPaths[optPath]
+    deficientPaths = []
+    for i in range(len(timeValues)):
+        if i > 0:
+            deficientPaths.append(subsetPathList(destinations,i))
+    subOptCosts = []
+    subOptPaths = []
+    subOptPaths.append(shortPath)
+    #first element of path list is shortest path
+    subOptPaths.append(optPath)
+    #second element of path list is optimal path
+    subOptCosts.append(indivCostMat(timeValues,optPath,distMatrix))
+    for i in range(len(timeValues)-1):
+        deficientOptPath = deficientPaths[i][optimalPath(timeValues,deficientPaths[i],distMatrix)]
+        subOptPaths.append(deficientOptPath)
+        #remaining elements of path list will be optimal paths leaving out one rider
+        subOptCosts.append(indivCostMat(timeValues, deficientOptPath,distMatrix))
+    output = []
+    output.append(subOptCosts[0])
+    for i in range(len(timeValues)):
+        if i > 0:
+            payments = subOptCosts[0]-subOptCosts[i]
+            payments[i] = 0
+            output.append(payments)
+
+    return output, subOptPaths, subOptCosts
+
+uberIncomesRaw = [25.0, 50.0, 85.0, 150.0, 300.0, 600.0]
+uberIncomesDistRaw = [8.0,23.0,18.0,27.0,9.0, 2.0]
+# Note that this doesn't sum to 100, because about 20% of uber drivers
+# declined to share their income with the surveyors
+
+uberIncomes = uberIncomesRaw/(np.ones(len(uberIncomesRaw)))
+# Note that these numbers are not normalized in any way
+# In production, these numbers should be in units of $/minute
+
+uberIncomesDist = uberIncomesDistRaw/np.sum(uberIncomesDistRaw)
+# Force probabilities to sum to 1.
+
+default_num_locations=5
+
+carCost = 15
+#Estimated car value of time to be equivalent to 15K/year earner
+def random_time_values(incomes, n, p, carCost=carCost):
+    timeValues = np.random.choice(incomes, n, p=p)
+    timeValues = np.append(carCost, timeValues)
+    timeValues *= (1000.0 / (262.0 * 8.0))
+    return(timeValues)
+
+
+def gen_time_values():
+    return random_time_values(uberIncomes, 5, uberIncomesDist)
+
+# In[14]:
+
+
+def get_info_from_inputs(num_locations=5, locationFile='locations.txt'):
+    inp_locations = readInputLocations(locationFile)
+    processed_array = randomLocations(inp_locations, num_locations)
+    response = sendRequest(processed_array)
+    addresses = response["origin_addresses"]
+    durMatrix = getDurationMatrix(response)
+    distMatrix = getDistanceMatrix(response)
+    return addresses, processed_array, durMatrix, distMatrix
+
+# addresses, lat_longs, durMatrix, distMatrix = get_info_from_inputs()
+
+# lat_longs = processed_array
+# # In[16]:
+
+# #rerandomize timevalues
+# timeValues = gen_time_values()
+# # timeValues
+
+
+# # In[17]:
+
+# timeValues = gen_time_values()
+# destinations = [(dest[0], dest[1], i) for i, dest in enumerate(processed_array)]
+# output, paths, costs = harmMatrices(timeValues, destinations, durMatrix)
+
+# # # In[18]:
+
+# destinations
+
+
+# In[19]:
+
+
+
+
+# In[20]:
+
+# costs
+
+
+# In[21]:
+
+# paths
+
+#path 0 is shortest
+#path 1 is optimal
--- run.py
+++ run.py
@@ -1,15 +1,27 @@
 import os, time
-from flask import Flask, Response, render_template, jsonify
+from flask import Flask, Response, render_template, jsonify, session
 from geojson import Point, Feature, FeatureCollection
-from distReq import randomLocations, readInputLocations, getDurationMatrix, getDistanceMatrix# uncomment this when using in another file
-
+from distReq import randomLocations, readInputLocations, getDurationMatrix, getDistanceMatrix, sendRequest# uncomment this when using in another file
+from findPath import gen_time_values, harmMatrices
+from mapbox import Directions
 
 
 app = Flask(__name__)
 app.debug = True
+destinationFeatureArray = []
+response = [];
+distMatrix = []
+durMatrix = []
+addreses = []
+paths = []
+costs = []
+output = []
+
+originFeature = []
+
 
 ACCESS_KEY = os.environ.get('MAPBOX_ACCESSKEY')
-
+service = Directions(ACCESS_KEY)
 @app.route('/')
 def index():
     return render_template('index.html', ACCESS_KEY=ACCESS_KEY)
@@ -17,26 +29,73 @@ def index():
 @app.route('/origin')
 def processOrigin():
     point = Point((-87.9073, 41.9742))
-    feature = Feature(geometry=point ,properties={'marker-color': '#3ca0d3','marker-size': 'large','marker-symbol': 'airport'})
-    feature_collection = FeatureCollection([feature])
+    originFeature = Feature(geometry=point ,properties={'marker-color': '#3ca0d3','marker-size': 'large','marker-symbol': 'airport'})
+    feature_collection = FeatureCollection([originFeature])
+    session['originFeature'] = originFeature
     return jsonify(result=feature_collection)
 
 @app.route('/dest')
 def processDestinations():
-    destinationFeatureArray = []
     numRiders = 5
     locationFile = "locations.txt" # stores all the lat/longs on different lines
     processedArray = randomLocations(readInputLocations(locationFile), numRiders)
-    # response = sendRequest(processedArray)
-    # addresses = response["origin_addresses"] # list of length numLocations of the addresses
+    destinationFeatureArray = []
     for i in range(1, len(processedArray)):
         point = Point((processedArray[i][1],processedArray[i][0]))
         feature =  Feature(geometry=point ,properties= {'marker-color': '#228B22','marker-size': 'medium','marker-symbol': chr(96+i)})
         destinationFeatureArray.append(feature)
-    # durMatrix = getDurationMatrix(response)
-    # distMatrix = getDistanceMatrix(response)
+    session['destinationFeatureArray']= destinationFeatureArray
     feature_collection = FeatureCollection(destinationFeatureArray)
+    response = sendRequest(processedArray,[])
+    session['addresses'] = addresses = response["origin_addresses"] # list of length numLocations of the addresses
+    session['durMatrix'] = durMatrix = getDurationMatrix(response)
+    session['distMatrix'] = distMatrix = getDistanceMatrix(response)
+    timeValues = gen_time_values()
+    destinations = [(dest[0], dest[1], i) for i, dest in enumerate(processedArray)]
+    output, paths, costs = harmMatrices(timeValues, destinations, durMatrix)
+    session['paths']=paths
     return jsonify(result=feature_collection)
+
+@app.route('/social')
+def socialRoute():
+    paths = session.get('paths', None)
+    originFeature = session.get('originFeature', None)
+    destinationFeatureArray = session.get('destinationFeatureArray', None)
+    socialPath = paths[0]
+    featureDict = []
+    featureDict.append(originFeature);
+    for i in range(1,len(socialPath)):
+        featureDict.append(destinationFeatureArray[socialPath[i][2]-1])
+    response2 = service.directions(featureDict, 'mapbox.driving')
+    print response2
+    responseJson = response2.geojson()
+    # responseJson['style'] = {"style":{ "fill":"red", "stroke-width":"3", "fill-opacity":0.6}}
+    # responseJson['features'][0]['properties']['fill']=  '#228B22'
+    responseJson['features'][0]['properties']['title']=  'Social Optimal Path'
+    responseJson['features'][0]['properties']['stroke']=  '#000080' # lime
+    responseJson['features'][0]['properties']['stroke-opacity']=  '0.7'
+    responseJson['features'][0]['properties']['stroke-width']=  '6'
+    return jsonify(result=responseJson)
+
+
+@app.route('/opt')
+def optRoute():
+    paths = session.get('paths', None)
+    originFeature = session.get('originFeature', None)
+    destinationFeatureArray = session.get('destinationFeatureArray', None)
+    optPath = paths[1]
+    featureDict = []
+    featureDict.append(originFeature);
+    for i in range(1,len(optPath)):
+        featureDict.append(destinationFeatureArray[optPath[i][2]-1])
+    response2 = service.directions(featureDict, 'mapbox.driving')
+    # print response2
+    responseJson = response2.geojson()
+    responseJson['features'][0]['properties']['title']=  'Utility Optimal Path'
+    responseJson['features'][0]['properties']['stroke']=  '#00FF00' # lime
+    responseJson['features'][0]['properties']['stroke-opacity']=  '0.7'
+    responseJson['features'][0]['properties']['stroke-width']=  '4'
+    return jsonify(result=responseJson)
 
 
 @app.route('/process')
@@ -52,6 +111,23 @@ def long_running_process2():
         yield 'data: Destination markers placed \n\n'
       return Response(generate2(), mimetype='text/event-stream')
 
+@app.route('/process3')
+def long_running_process3():
+      def generate3():
+        yield 'data: Social Path \n\n'
+      return Response(generate3(), mimetype='text/event-stream')
 
-if __name__ == '__main__':
-    app.run(debug = True)
+@app.route('/process4')
+def long_running_process4():
+      def generate4():
+        yield 'data: Optimal Path \n\n'
+      return Response(generate4(), mimetype='text/event-stream')
+
+
+
+if __name__ == "__main__":
+    app.secret_key = 'correctHorseBatteryStaple'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    app.debug = True
+    app.run()
